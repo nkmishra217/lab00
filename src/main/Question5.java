1970966b4630efc81b88db972931274eeba07f91
nkmishra217 <deeksha.mishra_cs18@gla.ac.in>
2019-08-25 10:37:41 +0530

    add:initialising variable p of string type

--- src/main/Question5.java
+++ src/main/Question5.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 public class Question5 {
     public void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String p = scanner.next().trim();
     }
 }
